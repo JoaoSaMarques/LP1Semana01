2ad1a3f145c74d811880a0909d666f73c509b27f
JoaoSaMarques <115433252+JoaoSaMarques@users.noreply.github.com>
2024-02-21 19:25:44 +0000

    added method to sum both numbers

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -10,13 +10,19 @@ namespace AskForInput
             Console.Writeline("Insira um número inteiro: ");
             string inputInteiroInt = Console.ReadLine();
 
+            // Convert string to int
             int i = int.Parse(inputInteiroInt);
 
+            // Peça ao utilizador para insesir um número real
             Console.Writeline("Insira um número real: ");
             string inputRealInt = Console.ReadLine();
 
+            // Convert string to float
             float f = float.Parse(inputRealInt);
 
+            // Soma
+            float soma = i + f;
+            Console.WriteLine($"Result of both numbers combined: {soma}");
         }
     }
 }
