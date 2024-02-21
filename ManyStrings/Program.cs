c6958796dbf8290e187b9d160dfeb0664d5c04b7
JoaoSaMarques <115433252+JoaoSaMarques@users.noreply.github.com>
2024-02-21 17:39:56 +0000

    added xx and ii, cases decimais e hexa + moeda em ordem

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -11,6 +11,8 @@ namespace ManyStrings
             string str2 = "SCREW THIS.\nWOOOOOORLD!!";
             string str3 = "Is\tThis\tA\tString?";
             string str4 = "Unicode: \u0001 \u0002 \u0003";
+            double xx = 3.1514;
+            int ii = 10;
 
             //Printing the values
             Console.WriteLine($"String 1: {str1}, {str2}, {str3}, {str4}");
@@ -21,6 +23,14 @@ namespace ManyStrings
             string conca2 = "abc" + x;
 
             Console.WriteLine($"Concatenacoes: {conca1}, {conca2}");
+
+            // Imprime xx
+            Console.WriteLine($"Número real com duas casas decimais: {xx:F2}");
+            Console.WriteLine($"Percentagem com uma casa decimal: {xx:P1}");
+
+            // Imprime ii 
+            Console.WriteLine($"Hexadecimal: 0x{ii:X}");
+            Console.WriteLine($"Moeda: {ii:C}");
         }
     }
 }
