b2b59073ea194009a8d58b092903238fd0a4e565
JoaoSaMarques <115433252+JoaoSaMarques@users.noreply.github.com>
2024-02-21 16:49:42 +0000

    Created new program based solely on printing different strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String variables
+            string str1 = "Hello, World!";
+            string str2 = "SCREW THIS.\nWOOOOOORLD!!";
+            string str3 = "Is\tThis\tA\tString?";
+            string str4 = "Unicode: \u0001 \u0002 \u0003";
+
+            //Printing the values
+            Console.WriteLine($"String 1: {str1}, {str2}, {str3}, {str4}");
+        }
+    }
+}
