96461de55d76258b3362632eccaa838fa2ed107e
JoaoSaMarques <115433252+JoaoSaMarques@users.noreply.github.com>
2024-02-21 16:08:42 +0000

    Added /// before program and main

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 
+
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Highest hierarchy of the program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main program of the file
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
