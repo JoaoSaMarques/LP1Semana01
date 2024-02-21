3f213ecf4f8e4fe7d8cb390ddb27f232932d460d
JoaoSaMarques <115433252+JoaoSaMarques@users.noreply.github.com>
2024-02-21 19:20:15 +0000

    fixed Console.writeline not having C and added user input for real number instead of inteiro

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,10 +7,13 @@ namespace AskForInput
         static void Main(string[] args)
         {
             // Peça ao utilizador para inserir um número inteiro
-            Console.Writeline("Insert a number: ");
-            string inputInt = Console.ReadLine();
+            Console.Writeline("Insira um número inteiro: ");
+            string inputInt = Console.ReadLine();C
 
             int i = int.Parse(inputInt)
+
+            Console.Writeline("Insira um número real: ");
+            string inputInt = Console.ReadLine();
 
         }
     }
