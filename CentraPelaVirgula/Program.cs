4a982423376896f261adb9f1000f8077f82cb494
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-23 14:12:15 +0000

    Converted string to float

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -13,21 +13,29 @@ namespace CentraPelaVirgula
             // Peça ao utilizador para insesir um número real
             Console.WriteLine("Insira um número real: ");
             string RealFirst = Console.ReadLine();
+            
+            float a = float.Parse(RealFirst);
 
             // Peça ao utilizador para insesir o segundo numero
             Console.WriteLine("Insira o segundo número real: ");
             string RealSecond = Console.ReadLine();
 
+            float b = float.Parse(RealSecond);
+
             // Peça ao utilizador para insesir o terceiro numero
             Console.WriteLine("Insira o terceiro número real: ");
             string RealThird = Console.ReadLine();
+
+            float c = float.Parse(RealThird);
 
             // Peça ao utilizador para insesir o quarto numero
             Console.WriteLine("Insira o quarto número real: ");
             string RealFourth = Console.ReadLine();
 
-            Console.WriteLine($"Nums:\n {RealFirst,6:F2}\n {RealSecond,6:F2}" +
-            $"\n {RealThird,6:F2}\n {RealFourth,6:F2}");
+            float d = float.Parse(RealFourth);
+
+            Console.WriteLine($"Nums:\n {a:f2}\n {b:f2}" +
+            $"\n {c:f2}\n {d:f2}");
         }
     }
 }
