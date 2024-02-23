da24a48fcce4a0b0fe6746abfe67eef727a5eff0
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-23 13:22:00 +0000

    Forth input

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -11,12 +11,16 @@ namespace CentraPelaVirgula
             string RealFirst = Console.ReadLine();
 
             // Peça ao utilizador para insesir o segundo numero
-            Console.WriteLine("Insira um segundo número real: ");
+            Console.WriteLine("Insira o segundo número real: ");
             string RealSecond = Console.ReadLine();
 
-            // Peça ao utilizador para insesir o segundo numero
-            Console.WriteLine("Insira um terceiro número real: ");
+            // Peça ao utilizador para insesir o terceiro numero
+            Console.WriteLine("Insira o terceiro número real: ");
             string RealThird = Console.ReadLine();
+
+            // Peça ao utilizador para insesir o quarto numero
+            Console.WriteLine("Insira o quarto número real: ");
+            string RealForth = Console.ReadLine();
         }
     }
 }
