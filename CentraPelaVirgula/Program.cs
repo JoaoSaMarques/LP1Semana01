77637d7be6008eb78c498d4fc8a6a2608f93fd23
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-02-23 13:20:45 +0000

    First input

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Peça ao utilizador para insesir um número real
+            Console.WriteLine("Insira um número real: ");
+            string RealFirst = Console.ReadLine();
+        }
+    }
+}
