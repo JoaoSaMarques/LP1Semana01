cd6fba3bf476cd2cc8dec207a8fb310797db7d75
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-19 19:03:19 +0000

    add variables type string and print

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -2,11 +2,27 @@
 
 namespace ManyStrings
 {
-    public class Program
+    class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            // Strings with escape characters
+            string str1 = "Hello\tWorld!";  // Tab character
+            string str2 = "Line 1\nLine 2";  // New line
+            string str3 = "Path: C:\\Users\\";  // Backslash
+            
+            // Strings with Unicode characters
+            string str4 = "Euro symbol: \u20AC";  // Euro symbol
+            string str5 = "Smiley: \u263A";  // Smiley face
+            string str6 = "Japanese: \u3042\u3044\u3046";  // Japanese characters
+            
+            // Printing all strings
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            Console.WriteLine(str4);
+            Console.WriteLine(str5);
+            Console.WriteLine(str6);
         }
     }
 }
