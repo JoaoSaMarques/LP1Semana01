764f6064943f17225664ccef5b62b7f7927906cf
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-19 16:04:59 +0000

    add comments to integer variables and char and real a nd add variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,6 +6,7 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            // Integer variables
             int i = 10;
             uint j = 20;
             short x = 0;
@@ -16,11 +17,21 @@ namespace VariousTypes
             Console.WriteLine(x);
             Console.WriteLine(l);
 
+            // Char variables
             char copyrightSymbol = '\u00A9';
             char newLine = '\u000A';
 
             Console.WriteLine(copyrightSymbol);
             Console.WriteLine(newLine);
+
+            // Real variables
+            float f = 3.1415f;
+            double d = 3.2025;
+            decimal m = 0.25m;
+
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(m);
 
         }
     }
