9b151a92c39d66fe0930f4aa7e9872f15a257359
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-19 15:37:17 +0000

    add /// before Main and class Program

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Beginning of the program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main function of the program
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
