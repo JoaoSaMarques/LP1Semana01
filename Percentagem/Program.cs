a08d6cd14e3f27aa03ba91b38a4fd0ca381b1839
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-21 14:03:04 +0000

    convert all 4 string to int

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -12,6 +12,11 @@ namespace Percentagem
             string S3 = Console.ReadLine();
             string S4 = Console.ReadLine();
 
+            int I1 = int.Parse(S1);
+            int I2 = int.Parse(S2);
+            int I3 = int.Parse(S3);
+            int I4 = int.Parse(S4);
+
         }
     }
 }
