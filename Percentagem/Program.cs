057c45047d78b3d4f033f8ffb981a0544ac90f3d
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-21 14:50:13 +0000

    add comments

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -9,22 +9,24 @@ namespace Percentagem
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
+            // Ask user for 4 real numbers
             Console.WriteLine("Insere 4 números reais entre 0 e 1:");
             string S1 = Console.ReadLine();
             string S2 = Console.ReadLine();
             string S3 = Console.ReadLine();
             string S4 = Console.ReadLine();
 
+            // Convert string of numbers to float
             float F1 = float.Parse(S1);
             float F2 = float.Parse(S2);
             float F3 = float.Parse(S3);
             float F4 = float.Parse(S4);
 
+            // Print floats into string to give % and align with padleft
             Console.WriteLine(F1.ToString("P2", CultureInfo.InvariantCulture).PadLeft(8));
             Console.WriteLine(F2.ToString("P2", CultureInfo.InvariantCulture).PadLeft(8));
             Console.WriteLine(F3.ToString("P2", CultureInfo.InvariantCulture).PadLeft(8));
             Console.WriteLine(F4.ToString("P2", CultureInfo.InvariantCulture).PadLeft(8));
-
         }
     }
 }
