cf8a60d8325f24a9d7d726edc0207dd84897efca
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-21 14:02:27 +0000

    ask for 4 real numbers and store them as string

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -6,7 +6,12 @@ namespace Percentagem
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Please input 4 real numbers:");
+            string S1 = Console.ReadLine();
+            string S2 = Console.ReadLine();
+            string S3 = Console.ReadLine();
+            string S4 = Console.ReadLine();
+
         }
     }
 }
