745630718c162c4a3f2f58d61eae19bd648cec66
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-02-21 13:36:19 +0000

    add comments

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,16 +6,21 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
+            // Ask for input of integer number
             Console.WriteLine("Please input an integer number:");
             string str = Console.ReadLine();
 
+            // Convert string to int
             int i = int.Parse(str);
 
+            // Ask for input of real number
             Console.WriteLine("Please input a real number:");
             string str2 = Console.ReadLine();
 
+            // Convert string to float
             float f = float.Parse(str2);
 
+            // Turn result into float
             float s = i + f;
 
             Console.WriteLine(s);
